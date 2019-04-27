cfd6d760d937f47c538c272793e389476a88109c
dipsdeepak11 <deepak.yadav@xoriant.com>
2019-04-27 14:47:14 +0530

    toggleContent, onChangeHandler, key issue resolved

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -6,42 +6,46 @@ import Person from './Person/Person'
 
 class App extends Component {
   state = {
-    person: [{
-        name: "x",age: 33},
-      {name: "y",age: 30},
+    person: [
+      {id: '1', name: "w",age: 24},
+      {id: '2', name: "x",age: 33},
+      {id: '3', name: "y",age: 30},
+      {id: '4', name: "z",age: 20},
+
     ],
     show: true
   }
 
-  switchButtonHandler = (NewName) => {
-    // console.log("was clicked")
-    this.setState({
-      person: [{
-          name: NewName,
-          age: 33
-        },
-        {
-          name: "y",
-          age: 50
-        },
-      ]
-    })
+  deleteContent = (PIndex) => {
+    // const newPerson = this.state.person;
+    const newPerson = [...this.state.person]
+    newPerson.splice(PIndex, 1);
+    this.setState({person:newPerson})
 
   }
 
-  onChangeHandler = (event) =>{
-    this.setState({
-      person: [{
-          name: event.target.value,
-          age: 33
-        },
-        {
-          name: "y",
-          age: 50
-        },
-      ]
+    onChangeHandler = (event, index) =>{
+    //finding the personid which need to be worked on
+    const personIndex = this.state.person.findIndex( (p) =>{
+      return p.id === index
     })
 
+    //getting the person on the respective personIndex
+    const selectedPerson = {
+      ...this.state.person[personIndex]
+    }
+
+    //updating the name of the selectedPerson
+    selectedPerson.name = event.target.value;
+
+    //need to update thw whole person with the updated selectedPerson
+    const allPerson = [...this.state.person]
+    allPerson[personIndex] = selectedPerson
+
+    //finally setting the person with the updatedperson
+    this.setState(
+      {person: allPerson}
+    )
 
   }
 
@@ -71,14 +75,27 @@ class App extends Component {
       //true condition
       personToggle = (
         <div>
-          <Person name = {this.state.person[0].name}
-          age = {this.state.person[0].age}
-          changed = {this.onChangeHandler} />
 
-          <Person name = {this.state.person[1].name}
-          age = {this.state.person[1].age}
-          click = {this.switchButtonHandler.bind(this,"mmm")}>
-          My Hobbies: Racing </Person>
+        {this.state.person.map((p, index) =>{
+          return <Person
+          name={p.name}
+          age={p.age}
+          key= {p.id}
+          click ={() => this.dsseleteContent(index)}
+          changed = {(event) => this.onChangeHandler(event, p.id)}/>
+        }
+        )}
+
+  {
+          // <Person name = {this.state.person[0].name}
+          // age = {this.state.person[0].age}
+          // changed = {this.onChangeHandler} />
+          //
+          // <Person name = {this.state.person[1].name}
+          // age = {this.state.person[1].age}
+          // click = {this.switchButtonHandler.bind(this,"mmm")}>
+          // My Hobbies: Racing </Person>
+        }
 
        </div>
      );
@@ -88,11 +105,7 @@ class App extends Component {
       <div className = "App" >
 
       <h1> This is Header </h1>
-      <button
-      style={buttonStyle}
-      onClick = {
-        this.switchButtonHandler.bind(this, "newname")
-      } > change name </button>
+
 
       <button
       style={buttonStyle}
--- react-complete-guide/src/Person/Person.js
+++ react-complete-guide/src/Person/Person.js
@@ -5,7 +5,7 @@ import "./Person.css"
 const person = (props) => {
   return (
     <div className="Person">
-    <p onClick={props.click}> Hi my name is {props.name} and I 'M {props.age} years old</p>
+    <p onClick={props.click}> Hi my name is <b>{props.name}</b> and I 'M {props.age} years old</p>
     <p> {props.children} </p>
     <input type= "text" onChange = {props.changed} value={props.name} />
     </div>
